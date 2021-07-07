8a358fb1bb4660d404c1f96ac8eef74307e7a616
yhcho0405 <yhcho0405@kakao.com>
2021-07-07 21:56:48 +0900

    initial commit

---
+++ philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youncho <youncho@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 21:41:07 by youncho           #+#    #+#             */
+/*   Updated: 2021/07/07 21:42:25 by youncho          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
